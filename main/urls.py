2608c2c42afe127d80685f3fa1f84d7939c2bc73
Saisnehan <k.saisnehan@gmail.com>
2026-07-03 02:46:30 +0530

    Add .gitignore and remove cache files

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+     path("download-resume/", views.download_resume, name="download_resume"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
+@login_required
+def download_resume(request):
+    return redirect(
+        "https://drive.google.com/file/d/165FpdgUpg-k8iJQzMbubir-ENeKUG1nO/view?usp=sharing"
+    )
