e93dd42be79c9c672f371d3e521af558e362e059
Saisnehan <k.saisnehan@gmail.com>
2026-07-04 01:10:17 +0530

    Fix CSRF_TRUSTED_ORIGINS syntax

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -253,22 +253,43 @@ SOCIALACCOUNT_PROVIDERS = {
 
 }
 
+import logging
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
         },
     },
+
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+
     "loggers": {
         "django": {
             "handlers": ["console"],
             "level": "DEBUG",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
         },
         "allauth": {
             "handlers": ["console"],
             "level": "DEBUG",
+            "propagate": False,
+        },
+        "allauth.socialaccount": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
         },
     },
 }
